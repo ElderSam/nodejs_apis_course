4f6219641d882a54f96b18e5ba76ec694d484723
ElderSam <eldersamuel98@gmail.com>
2022-01-30 14:47:02 -0300

    fix: add callbacks

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -4,28 +4,70 @@
     2 - Obter o endereço do usuário pelo Id
 */
 
-function getUser() {
+function getUser(callback) {
 	setTimeout(function () {
-		return {
+        // callback(error, success)
+
+		return callback(null, {
 			id: 1,
 			name: "Aladin",
 			birthDate: new Date(),
-		};
+		});
 	}, 1000);
 }
 
-function getPhoneById(userId) {
+function getPhone(userId, callback) {
 	setTimeout(() => {
-		return {
-			phone: "1199002",
+		return callback(null, {
+			phone: "997251020",
 			ddd: 11,
-		};
+		});
 	}, 2000);
 }
 
-function getAddressById(userId) {}
+function getAddress(userId, callback) {
+	setTimeout(() => {
+		return callback(null, {
+			street: "dos bobos",
+			number: 0,
+		});
+	}, 2000);
+}
 
-const user = getUser();
-// const phone = getPhoneById(user.id);
-console.log("user: ", user);
-// console.log("phone: ", phone);
+function resolveUser(error, user) {
+	console.log("user: ", user);
+}
+
+getUser(function resolveUser(error, user) {
+	//NOTE: ( null || "" || 0) === false (in Javascript)
+
+	if (error) {
+		console.error("DEU RUIM em USUÁRIO", error);
+		return;
+	}
+    console.log("user: ", user);
+
+	getPhone(user.id, function resolvePhone(error1, phone) {
+		if (error1) {
+			console.error("DEU RUIM em TELEFONE", error1);
+			return;
+		}
+
+		console.log("phone: ", phone);
+
+		getAddress(user.id, function resolveAddress(error2, address) {
+			if (error2) {
+				console.error("DEU RUIM em ENDEREÇO", error2);
+				return;
+			}
+
+			console.log("address: ", address);
+
+            console.log(`
+                Nome: ${user.name},
+                Telefone: (${phone.ddd}) ${phone.phone},
+                Endereço: Rua ${address.street}, número ${address.number}
+            `)
+		});
+	});
+});
