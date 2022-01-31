33ba158b6b8edc8cc605ba0164d7f4da7db625bc
ElderSam <eldersamuel98@gmail.com>
2022-01-31 08:13:38 -0300

    feat: events with stdin and addListener

--- 03-events/index.js
+++ 03-events/index.js
@@ -1,20 +1,29 @@
-const EventEmitter = require('events')
+// const EventEmitter = require('events')
 
-class MyEmitter extends EventEmitter { // Meu Emissor
+// class MyEmitter extends EventEmitter { // Meu Emissor
 
-}
+// }
 
-const myEmitter = new MyEmitter()
-const EventName = 'user:click'
+// const myEmitter = new MyEmitter()
+// const EventName = 'user:click'
 
-myEmitter.on(EventName, function (click) { // observador
-    console.log('um usuário clicou', click)
+// myEmitter.on(EventName, function (click) { // observador
+//     console.log('um usuário clicou', click)
+// })
+
+// myEmitter.emit(EventName, 'na barra de rolagem')
+// myEmitter.emit(EventName, 'no ok')
+
+// let count = 0
+// setInterval(() => {
+//     myEmitter.emit(EventName, 'no ok' + (count++))
+// }, 1000)
+
+
+/* ----------------------------------------------------------------- */
+const stdin = process.openStdin()
+console.log('digite algo')
+
+stdin.addListener('data', function (value) { // toda vez que digita uma entrada no terminal, dispara a função no addListener
+    console.log(`Você digitou: ${value.toString().trim()}`)
 })
-
-myEmitter.emit(EventName, 'na barra de rolagem')
-myEmitter.emit(EventName, 'no ok')
-
-let count = 0
-setInterval(() => {
-    myEmitter.emit(EventName, 'no ok' + (count++))
-}, 1000)
